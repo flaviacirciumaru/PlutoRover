75cbf9636a6e542ee3a4620c99719eb5861af59e
flaviacirciumaru <flaviacirciumaru@yahoo.com>
2019-02-12 17:19:43 +0200

    Create standard test for rover to move forward.

---
+++ PlutoTover_UnitTestProject/Grid.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PlutoTover_UnitTestProject
+{
+    internal class Grid
+    {
+        private int v1;
+        private int v2;
+        private List<KeyValuePair<int, int>> obstaclesPostion;
+
+        public Grid(int v1, int v2, List<KeyValuePair<int, int>> obstaclesPostion)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.obstaclesPostion = obstaclesPostion;
+        }
+    }
+}
--- PlutoTover_UnitTestProject/RoverPositionUnitTest.cs
+++ PlutoTover_UnitTestProject/RoverPositionUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace PlutoTover_UnitTestProject
@@ -7,8 +8,12 @@ namespace PlutoTover_UnitTestProject
     public class RoverPositionUnitTest
     {
         [TestMethod]
-        public void Move()
+        public void Move_Forward_NoEdge()
         {
+            /*planet info*/
+            /*the position of the obstacles is not important at this point*/
+            List<KeyValuePair<int, int>> obstaclesPostion = null;
+            Grid grid = new Grid(3, 3, obstaclesPostion);
         }
     }
 }
