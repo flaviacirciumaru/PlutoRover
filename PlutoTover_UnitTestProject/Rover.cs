3d12b6fca1e434bf66feacdf82a64547e569e783
flaviacirciumaru <flaviacirciumaru@yahoo.com>
2019-02-12 18:04:58 +0200

    Modify Rover class: -add method that check if valid letters are used as single commands - Move method has a switch based on the single-letter command

--- PlutoTover_UnitTestProject/Rover.cs
+++ PlutoTover_UnitTestProject/Rover.cs
@@ -7,6 +7,8 @@ namespace PlutoTover_UnitTestProject
         private Position currentPosition;
         private Grid grid;
 
+        private static bool noObstaclesFoundYet = true;
+
         public Rover(Position newPosition, Grid grid)
         {
             CurrentPosition = newPosition;
@@ -16,10 +18,69 @@ namespace PlutoTover_UnitTestProject
         
         internal Position CurrentPosition { get => currentPosition; set => currentPosition = value; } 
         internal Grid Grid { get => grid; set => grid = value; }
+        public static bool NoObstaclesFoundYet { get => noObstaclesFoundYet; set => noObstaclesFoundYet = value; }
 
-        internal void Move(char v)
+        internal void Move(char singleCommand)
+        {
+            VerifyIfCommandHasValidLetter(singleCommand);
+
+            if (NoObstaclesFoundYet)
+            {
+                switch (CurrentPosition.Orientation)
+                {
+                    case 'N':
+                        {
+                            FindNewPositionFacingNorth(singleCommand);
+                            break;
+                        }
+                    case 'S':
+                        {
+                            FindNewPositionFacingSouth(singleCommand);
+                            break;
+                        }
+                    case 'E':
+                        {
+                            FindeNewPositionFacingEast(singleCommand);
+                            break;
+                        }
+                    case 'W':
+                        {
+                            FindNewPositionFacingWest(singleCommand);
+                            break;
+                        }
+                }
+            }
+        }
+
+        private void FindNewPositionFacingWest(char singleCommand)
         {
             throw new NotImplementedException();
+        }
+
+        private void FindeNewPositionFacingEast(char singleCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FindNewPositionFacingSouth(char singleCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FindNewPositionFacingNorth(char singleCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void VerifyIfCommandHasValidLetter(char singleCommand)
+        {
+            if (!Equals(singleCommand, 'F') && !Equals(singleCommand, 'B')
+                && !Equals(singleCommand, 'L') && !Equals(singleCommand, 'R'))
+            {
+                throw new Exception("Rover can accept only F(forward)," +
+                    " B(backward), L (left) or R (right) commands." +
+                    "Please type a valid character.");
+            }
         }
     }
 }
